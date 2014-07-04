5c077a9413603cf321d1f49b41cacd4f5291aee1
Antmanler <antmanler@gmail.com>
2014-07-04 10:38:03 +0800

    Compile scss/sass on server

--- package.js
+++ package.js
@@ -1,17 +1,16 @@
 Package.describe({
-	summary: "Foundation 5 with Sass for Meteor"
+    summary: "Foundation 5 with Sass for Meteor"
 });
 
 Package.on_use(function (api) {
-	api.use([
-		'jquery',
-		'scss',
-	], 'client');
+    api.use("scss", ['client', 'server']);
+    api.imply('scss', ['client', 'server']);
+    api.use('jquery', 'client');
 
-	api.add_files([
+    api.add_files([
         'js/init.js',
         'js/vendor/modernizr.js',
         'js/foundation.js'
-	], 'client');
+    ], 'client');
 
 });
