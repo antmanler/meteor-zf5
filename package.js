96c19e44c99ad550a1ad773acff699ef516be957
Antmanler <antmanler@gmail.com>
2014-07-15 13:37:55 +0800

    Removed foundation's js files

--- package.js
+++ package.js
@@ -5,12 +5,12 @@ Package.describe({
 Package.on_use(function (api) {
     api.use("scss", ['client', 'server']);
     api.imply('scss', ['client', 'server']);
-    api.use('jquery', 'client');
+    // api.use('jquery', 'client');
 
-    api.add_files([
-        'js/init.js',
-        'js/vendor/modernizr.js',
-        'js/foundation.js'
-    ], 'client');
+    // api.add_files([
+    //     'js/init.js',
+    //     'js/vendor/modernizr.js',
+    //     'js/foundation.js'
+    // ], 'client');
 
 });
